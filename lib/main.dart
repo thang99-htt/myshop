a5d6065aaad1501654b46d1c6501a21f894d6a9a
THANG <thang99.h@gmail.com>
2023-01-19 13:40:40 +0700

    Xay dung trang cac dat hang

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'ui/products/product_detail_screen.dart';
 import 'ui/products/products_overview_screen.dart';
 import 'ui/products/user_products_screen.dart';
 import 'ui/cart/cart_screen.dart';
+import 'ui/orders/orders_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: const SafeArea(
-        child: CartScreen(),
+        child: OrdersScreen(),
       ),
     );
   }
