cf25a27426a046d9d3c13eb4a68c3cf8ab18414c
THANG <thang99.h@gmail.com>
2023-01-19 14:02:08 +0700

    Xay dung drawer va dinh tuyen theo ten

--- lib/ui/products/products_manager.dart
+++ lib/ui/products/products_manager.dart
@@ -48,4 +48,8 @@ class ProductsManager {
   List<Product> get favoriteItems {
     return _items.where((prodItem) => prodItem.isFavorite).toList();
   }
+
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
+  }
 }
--- lib/ui/products/products_overview_screen.dart
+++ lib/ui/products/products_overview_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../cart/cart_screen.dart';
 import 'products_grid.dart';
+import '../shared/app_drawer.dart';
 
 enum FilterOptions { favorites, all }
 
@@ -24,6 +26,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           buildShoppingCartIcon(),
         ],
       ),
+      drawer: const AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
@@ -34,7 +37,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
         Icons.shopping_cart,
       ),
       onPressed: () {
-        print('Go to cart screen');
+        Navigator.of(context).pushNamed(CartScreen.routeName);
       },
     );
   }
