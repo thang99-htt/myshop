d9c79052cbf917ce489938cbc8f6a6e808ef8bb2
THANG <thang99.h@gmail.com>
2023-01-19 11:49:07 +0700

    Xay dung trang tong quan cac san pham

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'ui/products/products_manager.dart';
 import 'ui/products/product_detail_screen.dart';
+import 'ui/products/products_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,10 +25,8 @@ class MyApp extends StatelessWidget {
           secondary: Colors.deepOrange,
         ),
       ),
-      home: SafeArea(
-        child: ProductDetailScreen(
-          ProductsManager().items[0],
-        ),
+      home: const SafeArea(
+        child: ProductsOverviewScreen(),
       ),
     );
   }
